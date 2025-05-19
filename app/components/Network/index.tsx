6d60ac5d99c4da501651daf99f2d8f2f8ce883b2
djvamps <dalopeza9@hotmail.com>
2025-05-18 19:55:05 -0500

    actualizacion codigo

--- app/components/Network/index.tsx
+++ app/components/Network/index.tsx
@@ -37,15 +37,30 @@ const Network = () => {
     return (
         <div className="bg-babyblue" id="project">
             <div className="mx-auto max-w-2xl py-20 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-                <h3 className="text-4xl sm:text-6xl font-semibold text-center my-10 lh-81">Our network & world <br /> work details.</h3>
+                <h3 className="text-4xl sm:text-6xl font-semibold text-center my-10 lh-81">
+                    Our network & world <br /> work details.
+                </h3>
 
-                <Image src={'/assets/network/map.png'} alt="map-image" width={1400} height={800} />
+                {/* Asegúrate de que la ruta sea correcta */}
+                <Image 
+                    src="/assets/network/map.png" 
+                    alt="World map" 
+                    width={1400} 
+                    height={800}
+                    className="w-full h-auto" // Estilos adicionales
+                />
 
                 <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-16 gap-y-4 lg:gap-x-8'>
                     {Aboutdata.map((item, i) => (
                         <div key={i} className='bg-white rounded-2xl p-5 shadow-xl'>
                             <div className="flex justify-start items-center gap-2">
-                                <Image src={item.imgSrc} alt={item.imgSrc} width={55} height={55} className="mb-2" />
+                                <Image 
+                                    src={item.imgSrc} 
+                                    alt={item.country} 
+                                    width={55} 
+                                    height={55} 
+                                    className="mb-2" 
+                                />
                                 <h4 className="text-xl font-medium text-midnightblue">{item.country}</h4>
                             </div>
                             <hr />
@@ -55,7 +70,9 @@ const Network = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Network;
+
+
